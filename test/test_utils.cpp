b9e15393a8d7d64f38dd0e33214d5af499af36eb
xunzhang <xunzhangthu@gmail.com>
2015-02-04 17:01:52 +0800

    sequential svd added

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -19,12 +19,16 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Sparse>
+#include <eigen3/Eigen/QR>
+#include <eigen3/Eigen/Cholesky>
 #include <boost/test/unit_test.hpp>
 #include "utils.hpp"
 #include "test.hpp"
 #include "paracel_types.hpp"
 
-BOOST_AUTO_TEST_CASE (json_parser_test) {
+BOOST_AUTO_TEST_CASE (paracel_json_parser_test) {
   paracel::json_parser pt("../../test/test.json");
   std::string r = "hong";
 
@@ -51,9 +55,213 @@ BOOST_AUTO_TEST_CASE (utils_hash_test) {
   BOOST_CHECK_EQUAL(hfunc(d), 3);
   paracel::hash_type<std::string> hfunc2;
   std::string x = "0", y = "1", z = "2", t = "3";
-  a = 2297668033614959926ULL; b = 10159970873491820195ULL; c = 4551451650890805270ULL; d = 8248777770799913213ULL;
+  a = 2297668033614959926ULL;
+  b = 10159970873491820195ULL;
+  c = 4551451650890805270ULL;
+  d = 8248777770799913213ULL;
   BOOST_CHECK_EQUAL(hfunc2(x), a);
   BOOST_CHECK_EQUAL(hfunc2(y), b);
   BOOST_CHECK_EQUAL(hfunc2(z), c);
   BOOST_CHECK_EQUAL(hfunc2(t), d);
+}
+
+BOOST_AUTO_TEST_CASE (eigen_common_usage_test) {
+  {
+    // Eigen::Matrix is static type while Eigen::MatrixXd is dynamic
+    Eigen::Matrix<double, 3, 3, Eigen::RowMajor> m = Eigen::Matrix3d::Identity();
+    m.col(1) = Eigen::Vector3d(4, 5, 6);
+    BOOST_CHECK_EQUAL(m.row(0)[0], 1);
+    BOOST_CHECK_EQUAL(m.row(0)[1], 4);
+    BOOST_CHECK_EQUAL(m.row(0)[2], 0);
+    BOOST_CHECK_EQUAL(m.row(1)[0], 0);
+    BOOST_CHECK_EQUAL(m.row(1)[1], 5);
+    BOOST_CHECK_EQUAL(m.row(1)[2], 0);
+    BOOST_CHECK_EQUAL(m.row(2)[0], 0);
+    BOOST_CHECK_EQUAL(m.row(2)[1], 6);
+    BOOST_CHECK_EQUAL(m.row(2)[2], 1);
+    BOOST_CHECK_EQUAL(m.IsRowMajor, true);
+    
+    auto v = paracel::mat2vec(m);
+    std::vector<double> check_v = {1, 0, 0, 4, 5, 6, 0, 0, 1};
+    BOOST_CHECK_EQUAL_V(v, check_v);
+    Eigen::MatrixXd check_mat = paracel::vec2mat(v, 3);
+    Eigen::VectorXd vv = m.col(1);
+    auto vvv = paracel::evec2vec(vv);
+    std::vector<double> check_vvv = {4, 5, 6};
+    BOOST_CHECK_EQUAL_V(vvv, check_vvv);
+    Eigen::VectorXd vvvv = paracel::vec2evec(vvv);
+
+    Eigen::Vector3d vec(1, 2, 3);
+    auto r = m.row(2) * vec;
+    BOOST_CHECK_EQUAL(r, 15);
+    
+    srand( (unsigned)time(NULL) );
+    Eigen::Matrix3d mm = Eigen::Matrix3d::Random();
+    std::cout << "matrix mm:" << std::endl << mm << std::endl;
+    std::cout << "sum of each column:" << std::endl << mm.colwise().sum() << std::endl;
+    std::cout << "sum of each row:" << std::endl << mm.rowwise().sum() << std::endl;
+    std::cout << "maximum absolute value of each column:" 
+        << std::endl << mm.cwiseAbs().colwise().maxCoeff() << std::endl;
+  }
+
+  {
+    Eigen::MatrixXd mtx(3, 2);
+    Eigen::VectorXd vech(2);
+    vech[0] = 1.1; vech[1] = 0.8;
+    mtx.row(0) = vech;
+    vech[0] = 2.; vech[1] = 2.;
+    mtx.row(1) = vech;
+    vech[0] = 1.; vech[1] = 0.5;
+    mtx.row(2) = vech;
+    vech[0] = 1.01; vech[1] = 0.28;
+    BOOST_CHECK_EQUAL(vech[0], 1.01);
+    BOOST_CHECK_EQUAL(vech[1], 0.28);
+    BOOST_CHECK_EQUAL(mtx.col(0)[0], 1.1);
+    BOOST_CHECK_EQUAL(mtx.col(0)[1], 2);
+    BOOST_CHECK_EQUAL(mtx.col(0)[2], 1);
+    BOOST_CHECK_EQUAL(mtx.col(1)[0], 0.8);
+    BOOST_CHECK_EQUAL(mtx.col(1)[1], 2);
+    BOOST_CHECK_EQUAL(mtx.col(1)[2], 0.5);
+    
+    Eigen::MatrixXd::Index indx;
+    (mtx.rowwise() - vech.transpose()).rowwise().squaredNorm().minCoeff(&indx);
+    BOOST_CHECK_EQUAL(indx, 2);
+    
+    auto result = mtx * vech; 
+    BOOST_CHECK_EQUAL(result.row(0)[0], 1.3350000000000002);
+    BOOST_CHECK_EQUAL(result.row(1)[0], 2.58);
+    BOOST_CHECK_EQUAL(result.row(2)[0], 1.15);
+    
+    mtx.row(0) *= 10;
+    BOOST_CHECK_EQUAL(mtx.row(0)[0], 11);
+    BOOST_CHECK_EQUAL(mtx.row(0)[1], 8);
+    
+    Eigen::Matrix2d mmat;
+    mmat << 1, 2, 
+            3, 4; 
+    Eigen::VectorXd vvec(2);
+    int rr = -1, cc = -1;
+    vvec << 1.1, 2.2;
+    BOOST_CHECK_EQUAL(mmat.maxCoeff(&rr, &cc), 4);
+    BOOST_CHECK_EQUAL(rr, 1);
+    BOOST_CHECK_EQUAL(cc, 1);
+    BOOST_CHECK_EQUAL(mmat.row(1).sum(), 7);
+  }
+}
+
+BOOST_AUTO_TEST_CASE (eigen_matrix_usage_test) {
+  
+  Eigen::MatrixXd mtx(4, 3);
+  mtx << 1., 2., 3.,
+        4., 5., 6.,
+        7., 8., 9.,
+        10., 11., 12.;
+  Eigen::MatrixXd clusters_mtx(2, 3);
+  std::vector<double> c1 = {2.5, 3.5, 4.5}, c2 = {8.5, 9.5, 10.5};
+  clusters_mtx.row(0) = paracel::vec2evec(c1);
+  clusters_mtx.row(1) = paracel::vec2evec(c2);
+
+  for(size_t i = 0; i < (size_t)mtx.rows(); ++i) {
+    Eigen::MatrixXd::Index indx;
+    (clusters_mtx.rowwise() - mtx.row(i)).rowwise().squaredNorm().minCoeff(&indx);
+  }
+  typedef Eigen::Triplet<double> eigen_triple;
+  std::vector<eigen_triple> tpls;
+  tpls.push_back(eigen_triple(0, 0, 0.6));
+  tpls.push_back(eigen_triple(0, 2, 0.7));
+  tpls.push_back(eigen_triple(0, 4, 0.4));
+  tpls.push_back(eigen_triple(1, 2, 0.6));
+  tpls.push_back(eigen_triple(1, 3, 0.5));
+  tpls.push_back(eigen_triple(1, 4, 0.3));
+  tpls.push_back(eigen_triple(2, 0, 0.3));
+  tpls.push_back(eigen_triple(2, 1, 0.1));
+  tpls.push_back(eigen_triple(3, 3, 0.1));
+  tpls.push_back(eigen_triple(3, 4, 0.7));
+  tpls.push_back(eigen_triple(4, 1, 0.3));
+  tpls.push_back(eigen_triple(5, 0, 0.1));
+  tpls.push_back(eigen_triple(5, 4, 0.7));
+  tpls.push_back(eigen_triple(6, 0, 0.2));
+  tpls.push_back(eigen_triple(6, 2, 0.8));
+  tpls.push_back(eigen_triple(7, 0, 0.3));
+  tpls.push_back(eigen_triple(8, 1, 0.1));
+  tpls.push_back(eigen_triple(8, 2, 0.2));
+  tpls.push_back(eigen_triple(8, 3, 0.3));
+  tpls.push_back(eigen_triple(8, 4, 0.4));
+  tpls.push_back(eigen_triple(9, 0, 0.9));
+  tpls.push_back(eigen_triple(9, 3, 0.1));
+  tpls.push_back(eigen_triple(9, 4, 0.2));
+  Eigen::SparseMatrix<double, Eigen::RowMajor> A;
+  A.resize(10, 5);
+  A.setFromTriplets(tpls.begin(), tpls.end());
+  Eigen::MatrixXd H(5, 3); // 5 * 3
+  H << 1., 2., 3.,
+      4., 5., 6.,
+      7., 8., 9.,
+      10., 11., 12.,
+      13., 14., 15.;
+  Eigen::MatrixXd W(1,1);
+  W.resize(10, 3);
+  W = A * H;
+  std::cout << W << std::endl;
+
+  Eigen::HouseholderQR<Eigen::MatrixXd> qr(W.transpose() * W);
+  Eigen::MatrixXd squareW(2, 2);
+  squareW << 1., 2., 3., 4.;
+  std::cout << squareW.inverse() << std::endl;
+  Eigen::MatrixXd R = qr.matrixQR().triangularView<Eigen::Upper>();
+  std::cout << R << std::endl;
+  std::cout << R.inverse() << std::endl;
+
+  Eigen::MatrixXd AA(3, 3);
+  AA <<  0.872871, -0.574833, -0.304016,
+      -0.574833, 0.827987, 0.120067,
+      -0.304016, 0.120067, 0.297787;
+  std::cout << "The matrix AA is" << std::endl << AA << std::endl;
+  Eigen::LLT<Eigen::MatrixXd> lltOfA(AA); // compute the Cholesky decomposition of A
+  Eigen::MatrixXd L = lltOfA.matrixL(); // retrieve factor L  in the decomposition
+  
+  // The previous two lines can also be written as "L = A.llt().matrixL()"
+  std::cout << "The Cholesky factor L is" << std::endl << -L << std::endl;
+  std::cout << "To check this, let us compute L * L.transpose()" << std::endl;
+  std::cout << L * L.transpose() << std::endl;
+  std::cout << "This should equal the matrix A" << std::endl;
+
+  Eigen::MatrixXd ma = Eigen::MatrixXd::Random(3,2);
+  std::cout << "Here is the matrix m:" << std::endl << ma << std::endl;
+  Eigen::JacobiSVD<Eigen::MatrixXd> svd(ma, Eigen::ComputeThinU | Eigen::ComputeThinV);
+  Eigen::MatrixXd SIGMA = svd.singularValues();
+  Eigen::MatrixXd U = svd.matrixU();
+  Eigen::MatrixXd V = svd.matrixV();
+  std::cout << SIGMA.rows() << SIGMA.cols() << U.rows() << U.cols() << V.rows() << V.cols() << std::endl;
+  Eigen::MatrixXd tttmmmppp = U * SIGMA;
+  std::cout << tttmmmppp * V.transpose() << std::endl;
+  std::cout << "Its singular values are:" << std::endl << SIGMA << std::endl;
+  std::cout << "Its left singular vectors are columns of the thin U matrix:" << std::endl << U << std::endl;
+  std::cout << "Its right singular vectors are columns of the thin V matrix:" << std::endl << V << std::endl;
+
+  std::cout.precision(20);
+  Eigen::MatrixXd H_blk(4, 3);
+  Eigen::MatrixXd stone = Eigen::MatrixXd::Random(2, 3);
+  H_blk.row(0) = stone.row(0);
+  H_blk.row(1) = stone.row(1);
+  H_blk.row(2) = stone.row(0);
+  H_blk.row(3) = stone.row(1);
+  std::cout << stone.transpose() * stone << std::endl;
+  auto kk = H_blk.block(0, 0, 2, 3).transpose() * H_blk.block(0, 0, 2, 3);
+  auto kk2 = H_blk.block(2, 0, 2, 3).transpose() * H_blk.block(2, 0, 2, 3);
+  Eigen::MatrixXd tt(3, 3);
+  tt << 
+    kk.row(0)[0] + kk2.row(0)[0],
+    kk.row(0)[1] + kk2.row(0)[1],
+    kk.row(0)[2] + kk2.row(0)[2],
+    kk.row(1)[0] + kk2.row(1)[0],
+    kk.row(1)[1] + kk2.row(1)[1],
+    kk.row(1)[2] + kk2.row(1)[2],
+    kk.row(2)[0] + kk2.row(2)[0],
+    kk.row(2)[1] + kk2.row(2)[1],
+    kk.row(2)[2] + kk2.row(2)[2];
+                            
+  std::cout << tt << std::endl;
+  std::cout << tt.inverse() << std::endl;
+  std::cout << (H_blk.transpose() * H_blk).inverse() << std::endl;
 }
---
+++ tool/svd_serial.cpp
@@ -0,0 +1,185 @@
+/**
+ * Copyright (c) 2014, Douban Inc. 
+ *   All rights reserved. 
+ *
+ * Distributed under the BSD License. Check out the LICENSE file for full text.
+ *
+ * Paracel - A distributed optimization framework with parameter server.
+ *
+ * Downloading
+ *   git clone https://github.com/douban/paracel.git 
+ *
+ * Authors: Hong Wu <xunzhangthu@gmail.com>
+ *
+ */
+
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <unordered_map>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Sparse>
+#include <eigen3/Eigen/QR>
+#include <eigen3/Eigen/Cholesky>
+#include <eigen3/Eigen/SVD>
+
+#include <google/gflags.h>
+
+#include "ps.hpp"
+#include "load.hpp"
+#include "utils.hpp"
+#include "paracel_types.hpp"
+
+using std::string;
+using std::vector;
+using paracel::paralg;
+using paracel::Comm;
+using paracel::str_split;
+
+namespace paracel {
+namespace tool {
+
+using node_t = paracel::default_id_type;
+
+class svd {
+
+public:  
+  svd(Comm comm,
+  	string _input, 
+  	string _output,
+    int _k) : input(_input), k(_k) {
+    pt = new paralg(comm, _output);
+    rank = pt->get_worker_id();
+    np = pt->get_worker_size();
+  }
+  
+  ~svd() {
+    delete pt;
+  } 
+
+  double loss(const Eigen::MatrixXd & A,
+              const Eigen::MatrixXd & result) {
+    return sqrt((A - result).squaredNorm() / (A.rows() * A.cols()));
+  }
+
+  Eigen::MatrixXd mf(const Eigen::MatrixXd & AA,
+                     Eigen::MatrixXd & WW,
+                     Eigen::MatrixXd & HH) {
+    HH = Eigen::MatrixXd::Random(AA.cols(), k);
+    for(int iter = 0; iter < 10; ++iter) {
+      auto tmp1 = HH.transpose() * HH;
+      WW = AA * HH * tmp1.inverse();
+      auto tmp2 = WW.transpose() * WW;
+      HH = AA.transpose() * WW * tmp2.inverse();
+    }
+    return WW * HH.transpose();
+  }
+
+  void qr_iteration(const Eigen::MatrixXd & M,
+                    Eigen::MatrixXd & q,
+                    Eigen::MatrixXd & r) {
+    Eigen::MatrixXd MtM = M.transpose() * M;
+    Eigen::LLT<Eigen::MatrixXd> lltOfA(MtM);
+    Eigen::MatrixXd L = lltOfA.matrixL();
+    r = -L.transpose();
+    q = -M * L.transpose().inverse();
+  }
+
+  void subsvd(const Eigen::MatrixXd & r_W,
+              const Eigen::MatrixXd & r_H,
+              Eigen::MatrixXd & S,
+              Eigen::MatrixXd & r_U,
+              Eigen::MatrixXd & r_V) {
+    //std::cout << "r_W * r_H.transpose() " << r_W * r_H.transpose() << std::endl;
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(r_W * r_H.transpose(),
+                                          Eigen::ComputeThinU | Eigen::ComputeThinV);
+    S = svd.singularValues(); // rank * 1
+    r_U = svd.matrixU();
+    r_V = svd.matrixV();
+  }
+
+  void solve() {
+    auto parser_lambda = [] (const string & line) {
+      return str_split(line, ',');
+    };
+    auto f_parser = paracel::gen_parser(parser_lambda);
+    pt->paracel_load_as_matrix(A, row_map, col_map, input, f_parser);
+
+    // first stage
+    Eigen::MatrixXd W, H;
+    auto approximate_A = mf(A, W, H);
+    //std::cout << "mf loss: " << loss(A, approximate_A);
+
+    // second stage
+    Eigen::MatrixXd q_W, q_H, r_W, r_H;
+    qr_iteration(W, q_W, r_W);
+    qr_iteration(H, q_H, r_H);
+    //std::cout << q_W * r_W * r_H.transpose() * q_H.transpose() << std::endl;
+    Eigen::MatrixXd r_U, r_V;
+    subsvd(r_W, r_H, sigma, r_U, r_V);
+    U = q_W * r_U;
+    V = q_H * r_V;
+    assert(U.cols() == k);
+    assert(V.cols() == k);
+  }
+
+  void dump_result() {
+    // dump sigma
+    std::vector<double> dump_sigma;
+    for(int i = 0; i < k; ++i) {
+      dump_sigma.push_back(sigma.row(i)[0]);
+    }
+    pt->paracel_dump_vector(dump_sigma, "SIGMA_");
+
+    // dump U
+    std::unordered_map<node_t, std::vector<double> > dump_u;
+    for(int i = 0; i < U.rows(); ++i) {
+      for(int j = 0; j < k; ++j) {
+        dump_u[row_map[i]].push_back(U.row(i)[j]);
+      }
+    }
+    pt->paracel_dump_dict(dump_u, "U_");
+
+    std::unordered_map<node_t, std::vector<double> > dump_v;
+    // dump V
+    for(int i = 0; i < V.rows(); ++i) {
+      for(int j = 0; j < k; ++j) {
+        dump_v[col_map[i]].push_back(V.row(i)[j]);
+      }
+    }
+    pt->paracel_dump_dict(dump_v, "V_");
+  }
+
+ private:
+  string input;
+  int k;
+  paralg *pt;
+  int rank, np;
+  Eigen::SparseMatrix<double, Eigen::RowMajor> A; // M * N
+  std::unordered_map<size_t, node_t> row_map, col_map;
+  Eigen::MatrixXd sigma, U, V;
+}; // class svd
+
+} // namespace tool 
+} // namespace paracel
+
+DEFINE_string(cfg_file, "", "config json file with absolute path.\n");
+
+int main(int argc, char *argv[])
+{
+  paracel::main_env comm_main_env(argc, argv);
+  paracel::Comm comm(MPI_COMM_WORLD);
+  
+  google::SetUsageMessage("[options]\n\t--cfg_file\n");
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  paracel::json_parser pt(FLAGS_cfg_file);
+  std::string input = pt.parse<std::string>("input");
+  std::string output = pt.parse<std::string>("output");
+  int k = pt.parse<int>("k");
+  paracel::tool::svd svd_solver(comm, input, output, k);
+  svd_solver.solve();
+  svd_solver.dump_result();
+  return 0;
+}
