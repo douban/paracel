32a32696483a9f8a99b96916692b051d52ef2e1c
Chongyu Zhu <i@lembacon.com>
2015-03-30 18:42:44 +0800

    alg/kmeans: Use `paracel::default_id_type` rather than `size_t`, so that it tracks the signature of `paracel_load_as_matrix`.

--- alg/clustering/kmeans/kmeans.hpp
+++ alg/clustering/kmeans/kmeans.hpp
@@ -190,7 +190,7 @@ class kmeans : public paracel::paralg {
 
   //Eigen::SparseMatrix<double, Eigen::RowMajor> blk_smtx;
   Eigen::MatrixXd blk_dmtx;
-  std::unordered_map<size_t, std::string> row_map; // matrix_indx -> id
+  std::unordered_map<paracel::default_id_type, std::string> row_map; // matrix_indx -> id
   std::vector<std::vector<double> > clusters;
   std::unordered_map<int, std::vector<std::string> > groups; // cluster_indx -> [ids]
 }; // class kmeans
