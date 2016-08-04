99dac69fae04d4cb76b600f993328debeed6cc6e
xunzhang <xunzhangthu@gmail.com>
2016-08-04 11:54:30 +0800

    [PARACEL-29]. Fix unittest failure under osx.

--- test/test_paracel_types.cpp
+++ test/test_paracel_types.cpp
@@ -31,15 +31,27 @@ BOOST_AUTO_TEST_CASE (paracel_types_test) {
   PARACEL_CHECK_EQUAL(true, paracel::is_seqic<std::vector<double>>::value);
 
   paracel::hash_type<std::string> hfunc;
+#if (defined __APPLE__)
+  PARACEL_CHECK_EQUAL(hfunc("abcd"), 8979402121050354847UL);
+#else
   PARACEL_CHECK_EQUAL(hfunc("abcd"), 16030370620903852840UL);
-
+#endif
+  
   std::vector<int> tmp2{1, 2, 3};
   paracel::hash_type< std::vector<int> > csfunc;
+#if (defined __APPLE__)
+  PARACEL_CHECK_EQUAL(csfunc(tmp2), 8405338360293734541UL);
+#else
   PARACEL_CHECK_EQUAL(csfunc(tmp2), 15152834183934801446UL);
+#endif
 
   std::vector<char> tmp3{'a', 'b', 'c'};
   paracel::hash_type< std::vector<char> > csfunc2;
+#if (defined __APPLE__)
+  PARACEL_CHECK_EQUAL(csfunc2(tmp3), 12863928803654259868UL);
+#else
   PARACEL_CHECK_EQUAL(csfunc2(tmp3), 1227721600474941649UL);
+#endif
 
   paracel::bag_type<int> bg;
   bg.put(1); bg.put(3); bg.put(5);
